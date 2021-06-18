32cf955ccd64d71bb0075c4479267d6e9a3b05a4
topba <66887206+topba@users.noreply.github.com>
2021-06-19 00:42:29 +0300

    ava'da recursive metotlar ile fibonacci serisi bulan program yapıyoru

---
+++ src/polin/Fibo.java
@@ -0,0 +1,14 @@
+package polin;
+
+public class Fibo {
+    public static void main(String[] args) {
+        System.out.println(fibo(7));
+    }
+
+    static int fibo(int n) {
+        if (n == 1 || n == 2) {
+            return 1;
+        }
+        return fibo(n - 1) + fibo(n - 2);
+    }
+}
