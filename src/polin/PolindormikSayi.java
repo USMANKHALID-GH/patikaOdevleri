1541e8818b6a9c42cec4b4de421f260ad2659b91
topba <66887206+topba@users.noreply.github.com>
2021-06-19 00:39:41 +0300

    Java ile bir sayının "Palindrom Sayı" olup olmadığını bulan bir program yapıyoruz.

---
+++ src/polin/PolindormikSayi.java
@@ -0,0 +1,17 @@
+package polin;
+
+public class PolindormikSayi {
+    public static void main(String[] args) {
+        System.out.println(isPalindrom(343));
+    }
+    static boolean isPalindrom(int number) {
+        int temp = number, reverseNumber = 0, mod;
+
+        while (temp != 0) {
+            mod = temp % 10;
+            reverseNumber = (reverseNumber * 10) + mod;
+            temp /= 10;
+        }
+        return number == reverseNumber;
+    }
+}
